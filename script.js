964290b72e4e8e55dc3fc4203047e62adebd332a
Jumi Yoon <jums.code@gmail.com>
2022-10-02 19:32:56 -0700

    added contact form

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const h5 = document.createElement("h5");
 h5.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h5);
 
+function jumiLight() {
+    document.getElementById("portrait").src = "jumi-2-mouseover.png";
+}
+
+function jumiOg(){
+    document.getElementById("portrait").src = "jumi-2-modified.png";
+}
